827161da749d09fdbd67e389e397a1d561360dde
msp4msps <msp4msps@tminus365.com>
2021-12-03 12:32:41 -0700

    updates to link

--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const blogRoutes = require("./blogRoutes");
-const commentRoutes = require("./commentRoutes");
+// const commentRoutes = require("./commentRoutes");
 
 router.use("/users", userRoutes);
-router.use("/posts", blogRoutes);
 
 module.exports = router;
