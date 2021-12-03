c40dd0154f0d7ff5c52301ca0e389b50b5618494
msp4msps <msp4msps@tminus365.com>
2021-12-03 12:27:30 -0700

    update file path

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
-const homeRoute = require("./homeRoutes");
+const homeRoutes = require("./homeRoutes.js");
 
-router.use("/", homeRoute);
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
