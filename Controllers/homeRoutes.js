9864d811c018655db3cc0f5d96baf3643605106e
msp4msps <msp4msps@tminus365.com>
2021-12-04 05:43:23 -0700

    get blog by id

--- Controllers/homeRoutes.js
+++ Controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { BlogPost, User } = require("../models");
+const session = require("express-session");
+const { BlogPost, User, Comments } = require("../models");
 
 //GET All Blog POST for Homepage
 
@@ -19,6 +20,39 @@ router.get("/", async (req, res) => {
       posts,
       username: req.session.username,
       user_id: req.session.user_id,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      message: err,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  // if (!req.session.loggedIn) {
+  //   res.redirect("/login");
+  //   return;
+  // }
+  try {
+    const dbBlogData = await BlogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comments,
+          attributes: ["commentBody", "created_date"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        { model: User, attributes: ["username"] },
+      ],
+    });
+    const post = dbBlogData.get({ plain: true });
+    req.session.post_id = post.id;
+    res.render("blogPost", {
+      post,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
